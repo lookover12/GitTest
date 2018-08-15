147d4ba5428080220173a567570e84bfa76525c7
lookover <384196232@qq.com>
2018-08-15 18:15:21 +0800

    Test a main

--- src/com/project/demo/Main.java
+++ src/com/project/demo/Main.java
@@ -3,6 +3,6 @@ package com.project.demo;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test git!");
+        System.out.println("Test git, test");
     }
 }
